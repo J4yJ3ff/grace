b7e4932dd49939a208869ba31f71d587b314ffcb
J4y_J3ff <77416100+J4yJ3ff@users.noreply.github.com>
2025-01-26 09:52:48 +0300

    fixes: hero

--- src/components/Hero-section.tsx
+++ src/components/Hero-section.tsx
@@ -3,19 +3,25 @@ import { Button } from "@/components/ui/button";
 
 export function HeroSection() {
   return (
-    <section className="px-4 py-14 grid grid-cols-1 md:grid-cols-2 items-center">
-      <div className="max-md:items-center  gap-4 flex flex-col">
-        <p className="text-sm font-bold text-gray-600 mb-2">
-          Branding | Logos | Graphic Design
-        </p>
-        <h1 className="text-6xl md:text-6xl font-bold mb-4">Visual Designer</h1>
+    <section className="px-8 py-14 grid grid-cols-1 md:grid-cols-2 items-center">
+      <div className="max-md:items-center flex flex-col">
+        <div>
+          <p className="font-bold mb-2">Branding | Logos | Graphic Design</p>
+          <h1 className="text-6xl md:text-6xl font-bold mb-4">
+            Visual Designer
+          </h1>
+        </div>
+
         <div className="hidden md:block">
-          <p className="text-gray-600 mb-8">
+          <p className=" mb-14">
             Create a strong, memorable brand with a custom design that brings
             your vision to life and engages your audience.
           </p>
-          <Button variant="default" className="bg-gray-900 hover:bg-gray-800">
-            Contact
+          <Button
+            variant="default"
+            className="px-16 py-8 rounded-none bg-gray-900 hover:bg-gray-800"
+          >
+            <p className="text-base">Contact</p>
           </Button>
         </div>
       </div>
