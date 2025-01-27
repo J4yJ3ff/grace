c5c0ec102ef590247bbfe490c2b942d2a0ada2be
J4y_J3ff <77416100+J4yJ3ff@users.noreply.github.com>
2025-01-27 03:21:16 +0300

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Header() {
   const navLinks = [
     {
@@ -16,19 +18,28 @@ export default function Header() {
 
   return (
     <header className="px-8 py-4 sm:flex sm:justify-between sm:flex-row sm:items-center items-center text-center ">
-      <div className="text-5xl font-semibold ">
-        grace<span className="text-[#FF6250]">.</span>
+      <div className="mb-4 sm:mb-0">
+        <Link
+          href="/"
+          aria-label="Grace - Home"
+          className="text-5xl font-semibold inline-block"
+        >
+          grace<span className="text-[#FF6250]">.</span>
+        </Link>
       </div>
-      <nav className="space-x-6 ">
-        {navLinks.map((link) => (
-          <a
-            key={link.title}
-            href={link.href}
-            className="text-gray-600 text-lg hover:text-[#FF6250] transition-colors max-w-sm:mb-2"
-          >
-            {link.title}
-          </a>
-        ))}
+      <nav aria-label="Main navigation">
+        <ul className="space-x-6 flex flex-wrap justify-center">
+          {navLinks.map((link) => (
+            <li key={link.title}>
+              <a
+                href={link.href}
+                className="text-gray-600 text-lg hover:text-[#FF6250] transition-colors inline-block"
+              >
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
